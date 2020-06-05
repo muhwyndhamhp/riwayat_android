291b8ea56c96cff2c494cbe480568f7391f96b6b
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-06-05 16:40:27 +0700

    create input case activity

---
+++ app/src/main/java/io/muhwyndhamhp/riwayat/ui/InputCaseActivity.kt
@@ -0,0 +1,13 @@
+package io.muhwyndhamhp.riwayat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.muhwyndhamhp.riwayat.R
+
+class InputCaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_case)
+    }
+}
