9b54c87148290a77fd91fd74b2ccef268e6e63e2
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-06-06 10:59:11 +0700

    adding gif as homescreen background

--- app/src/main/java/io/muhwyndhamhp/riwayat/ui/HomeActivity.kt
+++ app/src/main/java/io/muhwyndhamhp/riwayat/ui/HomeActivity.kt
@@ -3,8 +3,10 @@ package io.muhwyndhamhp.riwayat.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import io.muhwyndhamhp.riwayat.R
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -12,6 +14,8 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+//        val imageViewTarget = GlideDrawableImageViewTarget(iv_)
+        Glide.with(this).load(R.raw.one).into(iv_background)
         cv_anggota.setOnClickListener {
             startActivity(
                 Intent(
